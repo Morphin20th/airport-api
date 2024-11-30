5799a4cbaa37d083513b041bde1a4c394e56a78f
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-11-30 12:48:38 +0200

    add models User, Airplane and AirplaneType, refactor with black

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,25 @@
+from tkinter.font import names
+
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    def __str__(self):
+        return f"Airplane: {self.name}"
+
+    class Meta:
+        ordering = ["name"]
