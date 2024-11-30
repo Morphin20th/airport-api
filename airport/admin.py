6faf55a4369f96f01591c0f384c0161f128ee237
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-11-30 14:34:13 +0200

    feat 6 models, update .gitignore

--- airport/admin.py
+++ airport/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from airport.models import Airplane, AirplaneType
+from airport.models import (
+    Airplane,
+    AirplaneType,
+    Airport,
+    Route,
+    Flight,
+    Crew
+)
 
 admin.site.register(Airplane)
 admin.site.register(AirplaneType)
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(Flight)
+admin.site.register(Crew)
--- airport/models.py
+++ airport/models.py
@@ -1,5 +1,4 @@
-from tkinter.font import names
-
+from django.conf import settings
 from django.db import models
 
 
@@ -23,3 +22,70 @@ class Airplane(models.Model):
 
     class Meta:
         ordering = ["name"]
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    closest_big_city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}({self.closest_big_city})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes_from"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="routes_to"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"Route({self.source} - {self.destination})"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane, on_delete=models.CASCADE, related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"Flight {self.id}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders"
+    )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.created_at
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        Flight, on_delete=models.CASCADE, related_name="tickets"
+    )
+
+    def __str__(self):
+        return f"{self.flight.route} - row: {self.row} seat: {self.seat}"
